a358c8ddc989995eb2f1bf718fa9aa0cad75e3d3
dattran998 <76003578+dattran998@users.noreply.github.com>
2023-05-28 22:43:23 +0700

    day_08

---
+++ web/day_08/toto_web/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.slick-carousel').slick({
+        centerMode: true,
+        centerPadding: '200px',
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 500,
+        autoplay: true,
+        dots: false,
+        arrows: true,
+        autoplaySpeed: 2000,
+        
+    });
+  });
